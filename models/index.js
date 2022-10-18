3fa3829af50155e637178d4f62197ba2de22260c
소울치킨 <soulfever01@naver.com>
2022-10-18 18:16:37 +0900

    feat : jobPosting 모델 생성

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const Company = require("./company");
+const JobPosting = require("./jobPosting");
 
 require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
@@ -17,7 +18,9 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 
 db.Company = Company;
+db.JobPosting = JobPosting;
 
 Company.init(sequelize);
+JobPosting.init(sequelize);
 
 module.exports = db;
---
+++ models/jobPosting.js
@@ -0,0 +1,44 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+
+module.exports = class JobPosting extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        position: {
+          type: Sequelize.STRING(20),
+          allowNull: false, // not null
+          unique: false,
+        },
+        signingBonus: {
+          type: Sequelize.INTEGER,
+          allowNull: false, // not null
+          unique: false,
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: false, // not null
+          unique: false,
+        },
+        stack: {
+          type: Sequelize.STRING(20),
+          allowNull: false, // not null
+          unique: false,
+        },
+      },
+      {
+        // If don't want createdAt
+        createdAt: false,
+        // If don't want updatedAt
+        updatedAt: false,
+        sequelize,
+        underscored: true,
+        paranoid: false,
+        modelName: "JobPosting",
+        tableName: "job_posting",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+};
